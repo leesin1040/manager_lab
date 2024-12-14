a86308c18c77b99d699e4685987f707f29e53291
leesin1040 <leesin1040@gmail.com>
2024-12-15 04:32:40 +0900

    fix: 조금 수정

--- popup.js
+++ popup.js
@@ -1,14 +1,39 @@
 document.getElementById('runScript').addEventListener('click', () => {
+  const button = document.getElementById('runScript');
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.scripting.executeScript({
-      target: { tabId: tabs[0].id },
-      function: extractAllNamesAndCopyToClipboard,
-    });
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: tabs[0].id },
+        function: extractAllNames,
+      },
+      (results) => {
+        if (results && results[0] && results[0].result) {
+          const clipboardText = results[0].result.join('\n');
+          navigator.clipboard
+            .writeText(clipboardText)
+            .then(() => {
+              console.log('Copied to clipboard:', results[0].result);
+              button.textContent = '복사 완료!';
+              button.style.backgroundColor = '#2196F3';
+
+              // 3초 후 원래 텍스트로 복귀
+              setTimeout(() => {
+                button.textContent = '모든 이름 복사하기';
+                button.style.backgroundColor = '#4caf50';
+              }, 3000);
+            })
+            .catch((err) => {
+              console.error('Error copying to clipboard:', err);
+              button.textContent = '복사 실패';
+              button.style.backgroundColor = '#f44336';
+            });
+        }
+      }
+    );
   });
-  window.close();
 });
 
-function extractAllNamesAndCopyToClipboard() {
+function extractAllNames() {
   const containers = document.querySelectorAll('.css-856oyk');
   let names = [];
 
@@ -22,16 +47,5 @@ function extractAllNamesAndCopyToClipboard() {
     }
   });
 
-  const uniqueNames = [...new Set(names)];
-  const clipboardText = uniqueNames.join('\n');
-
-  navigator.clipboard
-    .writeText(clipboardText)
-    .then(() => {
-      console.log('Copied to clipboard:', uniqueNames);
-      alert('Names copied to clipboard!');
-    })
-    .catch((err) => {
-      console.error('Error copying to clipboard:', err);
-    });
+  return [...new Set(names)];
 }
